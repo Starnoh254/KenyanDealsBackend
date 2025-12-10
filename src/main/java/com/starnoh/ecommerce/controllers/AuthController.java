7be6bc5e7d56866bd9944ed243f3336b8e301787
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2025-12-10 15:09:13 +0300

    Add registration and login endpoints to AuthController

--- src/main/java/com/starnoh/ecommerce/controllers/AuthController.java
+++ src/main/java/com/starnoh/ecommerce/controllers/AuthController.java
@@ -1,9 +1,30 @@
 package com.starnoh.ecommerce.controllers;
 
+import com.starnoh.ecommerce.dto.AuthResponse;
+import com.starnoh.ecommerce.dto.LoginRequest;
+import com.starnoh.ecommerce.dto.RegisterRequest;
+import com.starnoh.ecommerce.service.AuthService;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public AuthResponse register(RegisterRequest request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(LoginRequest request) {
+        return authService.login(request);
+    }
 }
